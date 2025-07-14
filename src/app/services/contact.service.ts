5b271118915b13dc02a09e6a5515dee5eca5468a
UlisesNavarro01 <ulinavarromtz@gmail.com>
2025-07-14 15:23:54 -0600

    Configurar environment para URLs API

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';  // Importa environment
 
 interface ContactData {
   name: string;
@@ -12,7 +13,7 @@ interface ContactData {
   providedIn: 'root',
 })
 export class ContactService {
-  private apiUrl = 'http://127.0.0.1:8000/api/contact';  // Cambia aquí por tu endpoint
+  private apiUrl = `${environment.apiUrl}/contact`;  // Usa la URL del environment
 
   constructor(private http: HttpClient) {}
 
